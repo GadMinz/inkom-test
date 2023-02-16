bac1de2b130f5523fa3c1859072937c15f0164a0
GadMinz <baykov97@mail.ru>
2023-02-16 14:29:02 +0300

    Create project

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+import MainView from "@/views/MainView.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "main",
+      component: MainView,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      redirect: "/",
+    },
+  ],
+});
+
+export default router;
