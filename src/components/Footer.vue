22560f2fbf08e2151df22b55f9008180aa206446
GadMinz <baykov97@mail.ru>
2023-02-16 15:17:14 +0300

    Create footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="footer">
+    <div class="footer_content">
+      <RouterLink to="/favorites" class="footer_favorites">
+        Избранное
+      </RouterLink>
+      <a href="tel:+78008880088" class="footer_phone">8 800 888 00 88</a>
+    </div>
+  </footer>
+</template>
